0b34b139c06b048982be21744f7ddd262326f9ae
saori-eth <D84da@proton.me>
2024-03-19 10:57:46 -0500

    fix

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const getLatestMints = async () => {
     const mintResponse = await fetch(TOKEN_URL + mint);
     const mintData = await mintResponse.json();
     const { twitter_username } = mintData;
-    if (!twitter_username && !telegram)
+    if (!twitter_username || !telegram)
       return console.log(`No socials for ${name} token`);
     const info = {
       name,
